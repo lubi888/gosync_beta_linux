b57867c702be7533fba065e3456e95e68dfb9f55
lubi888 <linuxubquitous@gmail.com>
2025-12-04 21:17:12 +0000

    up to date working on Studio web and linux app

--- lib/l10n/app_localizations_de.dart
+++ lib/l10n/app_localizations_de.dart
@@ -9,7 +9,7 @@ class AppLocalizationsDe extends AppLocalizations {
   AppLocalizationsDe([String locale = 'de']) : super(locale);
 
   @override
-  String get titledate => 'goSync251130';
+  String get titledate => 'goSync251204';
 
   @override
   String get title => 'goSync de 240418 250528';
